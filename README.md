b5837a6aac23cc8e6e9b09b6120480c65555ade0
Tanmay Bhat <tanmaybhat24@gmail.com>
2024-09-12 22:39:26 +0530

    Init

---
+++ README.md
@@ -0,0 +1,17 @@
+### ArgoCD Plugin Generator Demo
+
+- The plugin service source code is in `/app`.
+- First, create the sample postgres service by running : 
+```
+kubectl apply -f postgres-manifests.yaml -n argocd
+```
+- Then, create the plugin service in k8s cluster by running : 
+```
+kubectl apply -f plugin-manifests.yaml -n argocd
+```
+- Finally, `/argocd` has the `applicationset.yaml` which can be applied ona k8s cluster that has ArgoCD installed by running : 
+```
+kubectl apply -f argocd/Applicationset.yaml -n argocd
+```
+
+Reference : https://argo-cd.readthedocs.io/en/stable/operator-manual/applicationset/Generators-Plugin/
---
+++ app/main.py
@@ -0,0 +1,50 @@
+import os
+import json
+from typing import List, Dict
+from fastapi import FastAPI, HTTPException, Depends, Security
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+import uvicorn
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+PLUGIN_TOKEN = os.getenv("AUTH_TOKEN")
+
+DB_PARAMS = {
+    'dbname': os.getenv('DB_NAME', 'organization'),
+    'user': os.getenv('DB_USER', 'postgres'),
+    'host': os.getenv('DB_HOST', 'localhost'),
+    'password': os.getenv('DB_PASSWORD','mysecretpassword'),
+    'port': os.getenv('DB_PORT', '5432')
+}
+
+app = FastAPI()
+security = HTTPBearer()
+
+async def query_postgres():
+    try:
+        conn = psycopg2.connect(**DB_PARAMS)
+        with conn.cursor(cursor_factory=RealDictCursor) as cur:
+            cur.execute("SELECT tenant, status FROM organization")
+            rows = cur.fetchall()
+        conn.close()
+        return [dict(row) for row in rows]
+    except Exception as e:
+        print(f"Database error: {e}")
+        return []
+
+def verify_token(credentials: HTTPAuthorizationCredentials = Security(security)):
+    if credentials.scheme != "Bearer" or credentials.credentials != PLUGIN_TOKEN:
+        raise HTTPException(status_code=403, detail="Invalid or missing token")
+    return credentials
+
+@app.post("/api/v1/getparams.execute")
+async def get_params_execute(credentials: HTTPAuthorizationCredentials = Depends(verify_token)):
+    tenants = await query_postgres()
+    return {
+        "output": {
+            "parameters": tenants
+        }
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=4355)
